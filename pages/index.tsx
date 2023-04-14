0db4898c0f0e438808516d2344d997bc14f8be76
ARCTICL1GHT <luis2009hervert@hotmail.com>
2023-04-14 11:06:41 -0600

    Agrega configuracion husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const msg = 'hello world';
+
 export default function Home() {
+  console.log(msg);
   return (
     <>
       <Head>
